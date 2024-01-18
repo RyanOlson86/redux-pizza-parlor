11ea691724d2997f7fb4b3f71c2f9fba0f680374
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-18 11:33:30 -0600

    created handleSubmit function

--- src/components/CustomerInfo/CustomerInfo.jsx
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react"
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 
 const CustomerInfo = () => {
+    const history = useHistory()
+    
+    // ! Global State
+    const dispatch = useDispatch()
+
+    // ! Local State
     const [customer, setCustomer] = useState({
         customer_name: '',
         street_address: '',
@@ -9,18 +16,24 @@ const CustomerInfo = () => {
         zip: '',
         type: 'Delivery'
     })
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        dispatch({type: 'ADD CUSTOMER', payload: customer})
+        history.push('/checkout')
+    }
     
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="nameInput">Name</label>
-                <input type="text" placeholder="Enter Name" id="nameInput" onChange={(event) => setCustomer({...customer, customer_name: event.target.value})} value={customer.customer_name}/>
+                <input required type="text" placeholder="Enter Name" id="nameInput" onChange={(event) => setCustomer({...customer, customer_name: event.target.value})} value={customer.customer_name}/>
                 <label htmlFor="streetInput">Street Address</label>
-                <input type="text" placeholder="Enter St. Address" id="streetInput" onChange={(event) => setCustomer({...customer, street_address: event.target.value})} value={customer.street_address}/>
+                <input required type="text" placeholder="Enter St. Address" id="streetInput" onChange={(event) => setCustomer({...customer, street_address: event.target.value})} value={customer.street_address}/>
                 <label htmlFor="cityInput">City</label>
-                <input type="text" placeholder="Enter City" id="cityInput" onChange={(event) => setCustomer({...customer, city: event.target.value})} value={customer.city}/>
+                <input required type="text" placeholder="Enter City" id="cityInput" onChange={(event) => setCustomer({...customer, city: event.target.value})} value={customer.city}/>
                 <label htmlFor="zipInput">Zip</label>
-                <input type="number" placeholder="Enter Zip Code" id="zipInput" onChange={(event) => setCustomer({...customer, zip: event.target.value})} value={customer.zip}/>
+                <input required type="number" placeholder="Enter Zip Code" id="zipInput" onChange={(event) => setCustomer({...customer, zip: event.target.value})} value={customer.zip}/>
                 
                 <input type="radio" id="Pickup" onChange={(event) => setCustomer({...customer, type: event.target.value})} value='Pickup' checked={customer.type === 'Pickup'}/>
                 <label htmlFor="Pickup">Pickup</label>
