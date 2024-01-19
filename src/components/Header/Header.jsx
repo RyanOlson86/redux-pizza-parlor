cb8bbb9bed48f013f04a1c26fee56020bafc7c6a
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-19 09:59:32 -0600

    started styling the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
+import { Typography } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
-import { Link , useHistory} from "react-router-dom/cjs/react-router-dom.min"
+import { useHistory} from "react-router-dom/cjs/react-router-dom.min"
 
 const Header = () => {
     const total = useSelector(store => store.total)
     const dispatch = useDispatch()
     const history = useHistory()
+    let USDollar = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+  })
 
     // Clicking on Header Title will reset cart and bring you back to index page
     const handleHome = ()=>{
@@ -14,8 +19,11 @@ const Header = () => {
 
   return (
     <header className='App-header'>
-        <span className='App-title' onClick={handleHome}>Prime Pizza</span>
-        <span>{total}</span>
+        {/* <span className='App-title' onClick={handleHome}>Prime Pizza</span> */}
+        {/* <h2 ></h2> */}
+        <Typography variant="h2" onClick={handleHome}>Prime Pizza</Typography>
+        <Typography variant="BUTTON TEXT"></Typography>
+        <span>Total: {USDollar.format(total)}</span>
     </header>
   )
 };
